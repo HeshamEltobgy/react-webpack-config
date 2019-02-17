8c1de85cd09cd8cd685d62c1edf85dd36996b6e4
Hesham Eltobgy <hesham.eltobgy@griffithuni.edu.au>
2019-02-17 23:14:13 +0800

    react-webpack-config ready

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+    test: /\.scss$/,
+    use: [
+      MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: true,
+          sourceMap: true
+        }
+      },
+      "sass-loader"
+    ]}
+    ]
+  },
+  plugins: [
+    // new HtmlWebPackPlugin({
+    //   template: "./src/index.html",
+    //   filename: "./index.html"
+    // }),
+    new MiniCssExtractPlugin({
+// Options similar to the same options in webpackOptions.output
+// both options are optional
+filename: "index.scss",
+chunkFilename: "[name].scss"
+})
+  ]
+};
