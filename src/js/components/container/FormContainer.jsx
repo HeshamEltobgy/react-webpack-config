37ad1e93b99d7424d4961014007e601c149a0b0a
Hesham Eltobgy <hesham.eltobgy@griffithuni.edu.au>
2019-02-18 10:59:20 +0800

    change style

--- src/js/components/container/FormContainer.jsx
+++ src/js/components/container/FormContainer.jsx
@@ -22,7 +22,7 @@ class FormContainer extends Component {
     return (
       <form id="article-form">
         <Input
-          text="SEO title"
+          text="Title"
           label="seo_title"
           type="text"
           id="seo_title"
